c1c733dccacfd6e36661466d199aa1b5ca16cc60
2017JTM2023 <vaibhavnigam50@gmail.com>
2017-09-12 16:16:47 +0530

    python code to check the parity bit

--- ps1.py
+++ ps1.py
@@ -3,12 +3,42 @@
 ####### write your code here ##########
 
 binary_data = input("Enter the binary data\n")
-
 flag ='0101'
+even= '1'
+even_parity= int(str(binary_data)+ str(even))
+even_parity_flag= int(str(binary_data)+ str(even)+ str(flag))
+odd= '0'
+odd_parity= int(str(binary_data)+ str(odd))
+odd_parity_flag= int(str(binary_data)+ str(odd)+ str(flag))
 
-z= int(str(binary_data)+ str(flag))
+def digitsum(binary_data):
+	total=0
+	for letter in str(binary_data):
+		total+=int(letter)
+		print(total)
+	
+	return total;
 
-print(z)
+	
+
+total=digitsum(binary_data)
+
+if total%2 == 0:
+	print("even number of 1's")
+	print("Binary data with parity bit")
+	print(even_parity)
+	print('The modified string received at the other end')
+	print(even_parity_flag)
+else:
+	print("odd number of 1's")
+	print("Binary data with parity bit")
+	print(odd_parity)
+	print('The modified string received at the other end')
+	print(odd_parity_flag)
+
+
+
+
 
 
 
