95b38f50993a3dd986bceaab9bd160d2b379d44d
2017JTM2023 <vaibhavnigam50@gmail.com>
2017-09-12 14:03:18 +0530

    python code to check the parity bit

--- ps1.py
+++ ps1.py
@@ -1,3 +1,13 @@
 ###### this is the first .py file ###########
 
 ####### write your code here ##########
+
+binary_data = input("Enter the binary data\n")
+
+flag = 0101
+
+print(binary_data,flag)
+
+
+
+
